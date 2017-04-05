080abd813b3b65ff69d214f0d78a9a71de9b52d5
doorknob <babaroga3000@gmail.com>
2017-04-05 13:07:28 +0300

    Dev branch, changing server.py, splitting _handle_response into header and body handlers

--- http_server/server.py
+++ http_server/server.py
@@ -9,6 +9,7 @@ logger.setLevel(logging.DEBUG)
 
 class HttpServer:
     def __init__(self, host, port):
+
         self.host = host
         self.port = port
 
@@ -31,46 +32,65 @@ class HttpServer:
                 self.socket.shutdown(socket.SHUT_RDWR)
 
     @staticmethod
-    def _handle_response(route, item_route):
+    def _handle_header(directory_route):
+
         header = 'HTTP/1.1 200 OK\n'
 
-        header += 'Content-Type: text/html\n'
+        if os.path.isfile(directory_route):
+            mime_type = mimetypes.guess_type(directory_route)[0]
 
-        directories = os.listdir(route)
-
-        if 'index.html' in directories:
-            logger.info('Found an index, on route %s.', route)
-            index_route = route + '/index.html'
-            with open(index_route, 'rb') as inf:
-                body = bytes.decode(inf.read())
+            if mime_type:
+                header += 'Content-Type: %s\n' % mime_type
+            else:
+                header += 'Content-Type: text/plain\n'
 
         else:
-            logger.info('Returning directory list on route %s.', item_route)
-
-            body_hat = '<html>\n<head>\n<title>Contents</title>\n<span >Directory listing for %s</span>\n<hr>\n \
-            </head>\n<body>\n<ul>' % item_route
-
-            body_center = ""
-
-            for item in directories:
-                body_center += "<li><a href='%s%s" % (item_route, item)
-                if os.path.isdir("%s/%s" % (route, item)):
-                    body_center += "/"
-                body_center += "'>%s</a></li>\n" % item
-
-            body_bottom = '</ul>\n<hr>\n</body>\n</html>'
-
-            body = body_hat + body_center + body_bottom
+            header += 'Content-Type: text/html\n'
 
         header += 'Connection: keep-alive\n\n'
 
-        logger.info('Header itself: \n %s' % header)
+        header = header.encode()
 
-        res = header + body
+        return header
 
-        return res
+    @staticmethod
+    def _handle_body(directory_route, request_route):
+
+        if os.path.isfile(directory_route):
+            with open(directory_route, 'rb') as inf:
+                body = inf.read()
+
+        else:
+            directories = os.listdir(directory_route)
+
+            if 'index.html' in directories:
+                logger.info('Found an index, on route %s.', directory_route)
+                index_route = directory_route + '/index.html'
+                with open(index_route, 'rb') as inf:
+                    body = inf.read()
+
+            else:
+                logger.info('Returning directory list on route %s.', request_route)
+
+                body_hat = '<html>\n<head>\n<title>Contents</title>\n<span >Directory listing for %s</span>\n<hr>\n \
+                </head>\n<body>\n<ul>' % request_route
+
+                body_center = ""
+
+                for item in directories:
+                    body_center += "<li><a href='%s%s" % (request_route, item)
+                    if os.path.isdir("%s/%s" % (directory_route, item)):
+                        body_center += "/"
+                    body_center += "'>%s</a></li>\n" % item
+
+                body_bottom = '</ul>\n<hr>\n</body>\n</html>'
+
+                body = (body_hat + body_center + body_bottom).encode()
+
+        return body
 
     def _handle_request(self):
+
         self.socket.listen(10)
         conn, addr = self.socket.accept()
 
@@ -86,34 +106,19 @@ class HttpServer:
 
             directory_route = os.getcwd() + request_route
 
-            if os.path.isfile(directory_route):
-                logger.info('Sending a file, route %s', directory_route)
+            header = self._handle_header(directory_route)
 
-                header = 'HTTP/1.1 200 OK\n'
+            body = self._handle_body(directory_route, request_route)
 
-                mime_type = mimetypes.guess_type(directory_route)[0]
+            response = header + body
 
-                if mime_type:
-                    header += 'Content-Type: %s\n' % mime_type
-                else:
-                    header += 'Content-Type: text/plain\n'
-
-                header += 'Connection: keep-alive\n\n'
-
-                conn.sendall(header.encode())
-
-                with open(directory_route, 'rb') as inf:
-                    conn.sendfile(inf)
-
-            else:
-                response = self._handle_response(directory_route, request_route).encode()
-
-                conn.send(response)
+            conn.send(response)
 
         finally:
             conn.close()
 
     def run(self):
+
         logger.info('Starting event loop.')
         while True:
             self._handle_request()
